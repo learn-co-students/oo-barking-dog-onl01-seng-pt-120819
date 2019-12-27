eed5d248542a989602a2f259624505cc2f934dc1
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-27 04:06:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+require 'pry'
+class Dog
+  def name=(the_dog_name)
+    @dog_name = the_dog_name
+  end
+  def name
+    @dog_name
+  end
+  def bark
+    puts "woof!"
+  end
+  def sit
+    puts "The dog is sitting"
+  end
+
+#  def name
+
+end# Your code goes here!
+
+fido = Dog.new
+fergi = Dog.new
+peter = Dog.new
+ed = Dog.new
+bill = Dog.new
+
+binding.pry
