df63141cda8241b918a4e85c9d8f52a632ab55f1
Brigette LeVert <bnl.levert@gmail.com>
2020-01-12 21:29:51 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+class Dog 
+  
+  # create setter method 
+  # the method takes an argument and has an instance variable equal to the argument.
+  def name=(dog_name)
+    @this_dog_name = dog_name
+  end
+  
+  # create getter method 
+  # the getter method sets the method name equal to the setter instance variable 
+  def name 
+    @this_dog_name
+  end 
+  
+  # create action method #bark 
+  #
+  def bark
+    puts "woof!"
+  end 
+  
+end 
+
+# instantiate new class instance 
+fido = Dog.new 
+
+# call name setter method on instance 
+fido.name = "Fido"
+
+# call bark method on instance 
+fido.bark
+
